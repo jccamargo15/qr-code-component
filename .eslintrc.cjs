fc7d4acef6f960cb4f2c652164daf1e8ba409e0b
José Carlos de Camargo <jccamargo15@gmail.com>
2024-01-05 23:51:43 -0300

    setting eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    '@rocketseat/eslint-config/react'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
